5285c2063a05c4b9ace293073b81649f8af0a531
Sergio Herrero <sergherrero@gmail.com>
2017-02-17 15:48:44 +0100

    Set imagenet models and files as default in settings.py.

--- settings.py
+++ settings.py
@@ -1,12 +1,15 @@
 import os
 import dotenv
 
-os.environ.update(dotenv.Dotenv('default.env'))
+if os.path.exists('default.env'):
+    os.environ.update(dotenv.Dotenv('default.env'))
 
 CATALOG_CONNECTION = os.environ.get('CATALOG_CONNECTION')
 CATALOG_QUERY_PATH = os.environ.get('CATALOG_QUERY_PATH')
 
 DATA_DIR = os.environ.get('DATA_DIR')
-MODEL_FILE = os.environ.get('MODEL_FILE')
-NODE_TO_UID_MAP_FILE = os.environ.get('NODE_TO_UID_MAP_FILE')
-UID_TO_LABEL_MAP_FILE = os.environ.get('UID_TO_LABEL_MAP_FILE')
+MODEL_FILE = os.environ.get('MODEL_FILE', 'classify_image_graph_def.pb')
+NODE_TO_UID_MAP_FILE = os.environ.get(
+    'NODE_TO_UID_MAP_FILE', 'imagenet_2012_challenge_label_map_proto.pbtxt')
+UID_TO_LABEL_MAP_FILE = os.environ.get(
+    'UID_TO_LABEL_MAP_FILE', 'imagenet_synset_to_human_label_map.txt')
